37cd3d352e8147133a5cb8f1d0489fcdaabdec39
Jose.Brandao <jose.sbrandao@gmail.com>
2019-07-15 22:17:25 -0300

    Inclui comentarios ao codigo

--- creating_objects_js.js
+++ creating_objects_js.js
@@ -1,8 +1,10 @@
+//Este é um exemplo de declaração de classe javascript
 function book(title,pages,isbn){//{1}}
   this.title = title;
   this.pages = page;
   this.isbn = isbn;
 }
+//Aqui estamos criando um método
 Book.prototype.printTitle = function(){
     console.log(this.title)
 }
