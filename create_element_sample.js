20d291b6ccedc2eb426f255f0d13f6e8172713f0
jose.sbrandao@gmail.com <jose.sbrandao@gmail.com>
2019-08-21 17:36:33 -0300

    adicionei o create element script

---
+++ create_element_sample.js
@@ -0,0 +1,7 @@
+var script = document.createElement('script');
+var script = document.createElement('head');
+script.type='text/javascript';
+sas_tmstp=Math.round(Math.random()*10000000000);
+console.log(sas_tmstp)
+script.innerHTML="<img src='https://www[intire URL HERE]'></img>";
+document.getElementsByTagName("head")[0].appendChild(script);
